5c3733e2933835ce772013b68ac5050e04e899d4
pedro mendez <prmendez222@gmail.com>
2026-07-26 19:37:52 -0600

    opcion 1 agregada

--- gasolinera.py
+++ gasolinera.py
@@ -32,40 +32,40 @@ class Historial:
     def cantidad_elementos(self):
         return len(self.historial)
 
-    #clase cola
-    class Cola:
-        def __init__(self):
-            self.cola=deque()
+#clase cola
+class Cola:
+    def __init__(self):
+        self.cola=deque()
 
-        def agregar(self,elemento):
+    def agregar(self,elemento):
             self.cola.append(elemento)
             print('elemento agregado a la cola')
-        def mostrar_primer_elemento(self):
-            if not self.cola:
-                print("no hay elementos")
-                return None
-            else:
-                return self.cola[0]
-        def eliminar_primer_elemento(self):
-            if not self.cola:
-                print("no hay elementos")
-                return None
-            else:
-                return self.cola.popleft()
-        def vaciar_cola(self):
-            if not self.cola:
-                print("no hay elementos")
-            else:
-                self.cola.clear()
-        def cantidad_en_cola(self):
-            return len(self.cola)
-        def mostrar_cola(self):
-            if not self.cola:
-                print("no hay elementos")
-            else:
-                for e in self.cola:
-                    print(e)
-                    print("=====================")
+    def mostrar_primer_elemento(self):
+        if not self.cola:
+            print("no hay elementos")
+            return None
+        else:
+            return self.cola[0]
+    def eliminar_primer_elemento(self):
+        if not self.cola:
+            print("no hay elementos")
+            return None
+        else:
+            return self.cola.popleft()
+    def vaciar_cola(self):
+        if not self.cola:
+            print("no hay elementos")
+        else:
+            self.cola.clear()
+    def cantidad_en_cola(self):
+        return len(self.cola)
+    def mostrar_cola(self):
+        if not self.cola:
+            print("no hay elementos")
+        else:
+            for e in self.cola:
+                print(e)
+                print("=====================")
 #clase empleado
 class Empleado:
     def __init__(self,id_empleado,nombre):
@@ -93,7 +93,7 @@ class Empleado:
         print("=====================================")
         print("ID: ",self.id)
         print("Nombre: ",self.nombre)
-
+#clase gasolinero
 class Gasolinero(Empleado):
     def __init__(self, id_gasolinero, nombre, bomba_que_atiende):
         super().__init__(id_gasolinero, nombre)
@@ -111,7 +111,7 @@ class Gasolinero(Empleado):
         super().mostrar_empleado()
         print("bomba que atiende: ",self.bomba_que_atiende)
         print("=====================================")
-
+#clase cajero
 class Cajero(Empleado):
     def __init__(self,id_cajero,nombre,numero_caja):
         super().__init__(id_cajero,nombre)
@@ -129,7 +129,7 @@ class Cajero(Empleado):
         super().mostrar_empleado()
         print("numero de caja: ",self.numero_caja)
         print("===================================")
-
+#clase bomba
 class Bomba:
     def __init__(self,id_bomba,tipo_servicio,tipo_combustible,precio):
         self.__id_bomba=id_bomba
@@ -137,6 +137,7 @@ class Bomba:
         self.__tipo_combustible=tipo_combustible
         self.__capacidad=1000
         self.__precio=precio
+        print("bomba creada con exito")
     @property
     def id(self):
         return self.__id_bomba
@@ -167,6 +168,24 @@ class Bomba:
             self.__precio=nuevo_precio
         else:
             raise ValueError("solo se permiten numeros decimales")
+        return
+    @property
+    def capacidad(self):
+        return self.__capacidad
+    @capacidad.setter
+    def capacidad(self,nueva_capacidad):
+        if not isinstance(nueva_capacidad,int):
+            raise ValueError("la capacidad debe ser expresada en numeros enteros")
+        elif nueva_capacidad>1000:
+            raise ValueError("la cantidad no debe ser mayor a 1000 galones")
+        elif nueva_capacidad<0:
+            raise ValueError("la cantidad no puede ser negativa")
+        else:
+            self.__capacidad=nueva_capacidad
+
+    def llenar_bomba(self,cantidad):
+        self.__capacidad=self.__capacidad+cantidad
+        
     def mostrar_bomba(self):
         print("========================")
         print("Id: ",self.id)
@@ -174,6 +193,7 @@ class Bomba:
         print("tipo de combustible: ",self.tipo_combustible)
         print("Precio: ",self.precio)
         print("=====================================")
+#clase cliente
 class Cliente:
     def __init__(self,tipo_servicio,tipo_combustible,cantidad):
         self.__tipo_servicio=tipo_servicio
@@ -200,7 +220,7 @@ class Cliente:
             self.__cantidad=nueva_cantidad
         else:
             raise ValueError("la cantidad debe ser un numero entero o decimal")
-
+#clase pago
 class Pago(Bomba, Cliente):
     def __init__(self, id_bomba, tipo_servicio, tipo_combustible, tipo_servicio_cliente, tipo_combustible_cliente, precio, cantidad, pago_recibido):
         Bomba.__init__(self, id_bomba, tipo_servicio, tipo_combustible, precio)
@@ -236,3 +256,134 @@ class Pago(Bomba, Cliente):
         print("pago recibido: ", self.pago)
         print("vuelto: ", round(self.calcular_vuelto(), 2))
         print("==============================================")
+
+cola_bomba_1=Cola()
+cola_Bomba_2=Cola()
+Cola_bomba_3=Cola()
+
+def agregar_cliente():
+    print("=======cliente en cola=========")
+    while True:
+        print("-------ingrese tipo de servicio-------")
+        print("1. servicio Completo")
+        opcion1=input("ingrese una opcion: ")
+        match opcion1:
+            case "1":
+                tipo_servicio="servicio_completo"
+                break
+            case _:
+                print("opcion no valida")
+
+    while True:
+        print("-------------tipo de combustible----------")
+        print("1. regular")
+        print("2. diesel")
+        print("3. super")
+        opcion2 = input("ingrese una opcion: ")
+        match opcion2:
+            case "1":
+                tipo_combustible = "regular"
+                break
+            case "2":
+                tipo_combustible = "diesel"
+                break
+            case "3":
+                tipo_combustible = "Super"
+                break
+            case _:
+                print("opcion no valida")
+
+    while True:
+        try:
+            cantidad=float(input("ingrese la cantidad (Q.): "))
+            break
+        except ValueError:
+            print("error: cantidad no aceptada")
+
+    while True:
+        try:
+            cliente1=Cliente(tipo_servicio,tipo_combustible,cantidad)
+            if tipo_combustible=="regular":
+                cola_bomba_1.agregar(cliente1)
+            elif tipo_combustible=="diesel":
+                cola_Bomba_2.agregar(cliente1)
+            elif tipo_combustible=="super":
+                Cola_bomba_3.agregar(cliente1)
+            break
+        except ValueError as e:
+            print("error: ",e)
+
+
+bombas=[]
+id_bomba=0
+def crear_bomba():
+    while True:
+        print("==================")
+        print("----------ingrese el tipo de servicio-------")
+        print("1. servicio completo")
+        opcion1=input("ingrese una opcion: ")
+        match opcion1:
+            case "1":
+                tipo_servicio="servicio completo"
+                break
+                break
+            case _:
+                print("opcion no valido")
+
+    while True:
+        print("-------------ingrese el tipo de combustible-------------")
+        print("1. regular")
+        print("2. diesel")
+        print("3. super")
+        opcion2=input("ingrese una opcion: ")
+        match opcion2:
+            case "1":
+                tipo_combustible="regular"
+                break
+            case "2":
+                tipo_combustible="diesel"
+                break
+            case "3":
+                tipo_combustible="super"
+                break
+            case _:
+                print("opcion no valida")
+    while True:
+        try:
+           precio=float(input("ingrese precio por galon: "))
+           break
+        except ValueError:
+            ValueError("el numero debe ser entero o dec")
+
+    while True:
+        try:
+            bomba1=Bomba(id_bomba,tipo_servicio,tipo_combustible,precio)
+            bombas.append(bomba1)
+            break
+        except ValueError as e:
+            print("error: ",e)
+
+while True:
+    print("1. ingreso de cliente")
+    print("2. atender cliente")
+    print("3. ver al al ultimo cliente atendido")
+    print("4. ver ultima factura hecha")
+    print("5. llenar bomba")
+    print("6. ver ultima bomba llenada")
+    print("7. ver historial de llenado")
+    print("8. ver historial de facturacion")
+    print("9. ingresar empleado")
+    print("10. crear una nueva bomba")
+    print("11. salir")
+    opcion=input("\n ingrese una opcion: ")
+    match opcion:
+        case "1":
+            agregar_cliente()
+        case "2":
+            for b in bombas:
+                b.mostrar_bomba()
+        case "10":
+            crear_bomba()
+            id_bomba=id_bomba+1
+        case "11":
+            break
